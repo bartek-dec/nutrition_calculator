6d9ac3118f9df50b892ee40a564998c9ad5449b9
Bartek Dec <decbartek@gmail.com>
2020-12-09 15:25:52 +0100

    initial commit

---
+++ src/main/java/com/example/nutritioncalculator/NutritioncalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutritioncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritioncalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritioncalculatorApplication.class, args);
+    }
+
+}
