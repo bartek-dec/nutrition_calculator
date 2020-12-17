ef60210ee2d67d753034c7a1e762dfa521a2af50
Bartek Dec <decbartek@gmail.com>
2020-12-17 16:04:01 +0100

    added method to register RestTemplate as a bean

--- src/main/java/com/example/nutritioncalculator/NutritioncalculatorApplication.java
+++ src/main/java/com/example/nutritioncalculator/NutritioncalculatorApplication.java
@@ -2,12 +2,19 @@ package com.example.nutritioncalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NutritioncalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NutritioncalculatorApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
